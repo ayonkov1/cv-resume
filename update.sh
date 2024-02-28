78122e0b6fd46314b00699aafed3349a5740a766
Atanas Yonkov <atanasyonkov1@gmail.com>
2024-02-28 20:32:27 +0100

    Update

--- update.sh
+++ update.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Run pdflatex to generate the PDF
-docker run --rm -i -v "$PWD":/data latex pdflatex main.tex
-
 # Convert the PDF to PNG
 convert -density 600 main.pdf -alpha extract -alpha copy -channel rgb -negate +channel main.png
 
